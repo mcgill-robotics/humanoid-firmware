2d5c76eddb2876e295ff23b3cfa1679f4b627bea
Your Name <peterwayne3293@gmail.com>
2024-02-13 23:13:09 -0500

    changed to microros_arduino

--- teensy41_microros_test/src/main.cpp
+++ teensy41_microros_test/src/main.cpp
@@ -1,106 +1,41 @@
 #include <Arduino.h>
-#include <NativeEthernet.h>
-#include <micro_ros_platformio.h>
+#include "user_config.h" // It must be located above ros2arduino.h.
+#include <ros2arduino.h>
 
-// #include <micro_ros_arduino.h>
+#define XRCEDDS_PORT Serial
+#define PUBLISH_FREQUENCY 2 // hz
 
-#include <stdio.h>
-#include <rcl/rcl.h>
-#include <rcl/error_handling.h>
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
-#include <std_msgs/msg/int32.h>
-
-#if !defined(TARGET_STM32F4) && !defined(ARDUINO_TEENSY41) && !defined(TARGET_PORTENTA_H7_M7)
-#error This example is only available for Arduino Portenta, Arduino Teensy41 and STM32F4
-#endif
-
-#if defined(ARDUINO_TEENSY41)
-void get_teensy_mac(uint8_t *mac)
+void publishString(std_msgs::String *msg, void *arg)
 {
-  for (uint8_t by = 0; by < 2; by++)
-    mac[by] = (HW_OCOTP_MAC1 >> ((1 - by) * 8)) & 0xFF;
-  for (uint8_t by = 0; by < 4; by++)
-    mac[by + 2] = (HW_OCOTP_MAC0 >> ((3 - by) * 8)) & 0xFF;
+  (void)(arg);
+
+  static int cnt = 0;
+  sprintf(msg->data, "Hello ros2arduino %d", cnt++);
 }
-#endif
 
-rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
-rclc_support_t support;
-rcl_allocator_t allocator;
-rcl_node_t node;
-
-#define LED_PIN 13
-
-#define RCCHECK(fn)              \
-  {                              \
-    rcl_ret_t temp_rc = fn;      \
-    if ((temp_rc != RCL_RET_OK)) \
-    {                            \
-      error_loop();              \
-    }                            \
-  }
-#define RCSOFTCHECK(fn)          \
-  {                              \
-    rcl_ret_t temp_rc = fn;      \
-    if ((temp_rc != RCL_RET_OK)) \
-    {                            \
-    }                            \
-  }
-
-void error_loop()
+class StringPub : public ros2::Node
 {
-  while (1)
+public:
+  StringPub()
+      : Node("ros2arduino_pub_node")
   {
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-    delay(100);
+    ros2::Publisher<std_msgs::String> *publisher_ = this->createPublisher<std_msgs::String>("arduino_chatter");
+    this->createWallFreq(PUBLISH_FREQUENCY, (ros2::CallbackFunc)publishString, nullptr, publisher_);
   }
-}
+};
 
 void setup()
 {
-  byte arduino_mac[] = {0xAA, 0xBB, 0xCC, 0xEE, 0xDD, 0xFF};
+  XRCEDDS_PORT.begin(115200);
+  while (!XRCEDDS_PORT)
+    ;
 
-#if defined(ARDUINO_TEENSY41)
-  get_teensy_mac(arduino_mac);
-#endif
-
-  // IPAddress arduino_ip(192, 168, 1, 177);
-  // set_microros_native_ethernet_udp_transports(arduino_mac, arduino_ip, agent_ip, 9999);
-  byte local_mac[] = {0xAA, 0xBB, 0xCC, 0xEE, 0xDD, 0xFF};
-  IPAddress local_ip(192, 168, 1, 177);
-  IPAddress agent_ip(192, 168, 1, 113);
-  size_t agent_port = 8888;
-
-  set_microros_native_ethernet_transports(local_mac, local_ip, agent_ip, agent_port);
-
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
-
-  delay(2000);
-
-  allocator = rcl_get_default_allocator();
-
-  // create init_options
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-
-  // create node
-  RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_ethernet_node", "namespace", &support));
-
-  // create publisher
-  RCCHECK(rclc_publisher_init_best_effort(
-      &publisher,
-      &node,
-      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-      "topic_name"));
-
-  msg.data = 0;
+  ros2::init(&XRCEDDS_PORT);
 }
 
 void loop()
 {
-  RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-  msg.data++;
-  delay(100);
+  static StringPub StringNode;
+
+  ros2::spin(&StringNode);
 }
