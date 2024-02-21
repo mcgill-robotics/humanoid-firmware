22d78e8f4659f103aa1946842d6271f38c66e49b
O O <peterwayne3293@gmail.com>
2024-02-20 22:33:39 -0500

    Servo node WIP

---
+++ ros_docker_test/noetic/humanoid_ws/src/servo_node/src/ServoJoint.py
@@ -0,0 +1,9 @@
+class ServoJoint:
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+        self.setpoint_raw = 0
+        self.setpoint_deg = 0
+        self.pos_raw = 0
+        self.pos_deg = 0
+    
