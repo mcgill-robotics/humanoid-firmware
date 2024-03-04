0ebae0957728fdec2822d9d3dc24a199c61c367f
Your Name <peterwayne3293@gmail.com>
2024-03-03 19:28:00 -0500

    ...

--- ros_docker_test/noetic/humanoid_ws/src/servo_node/src/FirmwareNode.py
+++ ros_docker_test/noetic/humanoid_ws/src/servo_node/src/FirmwareNode.py
@@ -1,6 +1,12 @@
+import os, sys
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(currentdir)
+
 import rospy
 import random
 import threading
+import math
 
 from ServoJoint import ServoJoint
 from servo_node.msg import ServoCommand, ServoFeedback
@@ -13,7 +19,7 @@ class FirmwareNode:
             "left_leg_ankle": ServoJoint(),
             "left_leg_knee": ServoJoint(),
             "left_leg_hip_pitch": ServoJoint(),
-            "left_leg_hip_roll": ServoJoint()
+            "left_leg_hip_roll": ServoJoint(),
         }
 
         rospy.init_node("FirmwareNode")
@@ -24,7 +30,9 @@ class FirmwareNode:
         )
 
         # Publishers
-        self.command_publisher = rospy.Publisher("/servosCommand", ServoCommand, queue_size=1)
+        self.command_publisher = rospy.Publisher(
+            "/servosCommand", ServoCommand, queue_size=1
+        )
 
         # Frequency of the Servo I/O
         self.rate = rospy.Rate(100)
@@ -45,19 +53,36 @@ class FirmwareNode:
         self.watchdog_thread = threading.Thread(target=self.watchdog_func)
         self.watchdog_thread.start()
 
+        last_time = rospy.get_time()
+
+        amplitude = 15  # Amplitude of the sine wave
+        frequency = 1  # Frequency in Hz
+
+        # Define phase shifts for each motor in radians. Example: {'motor1': 0, 'motor2': math.pi / 2, ...}
+        phase_shifts = {
+            name: random.uniform(0, 2 * math.pi) for name in self.joint_dict.keys()
+        }
+
         # MAIN LOOP
         while not rospy.is_shutdown():
             # PRINT TIMESTAMP
-            print(f"Time: {rospy.get_time()}")
+            current_time = rospy.get_time()
+            print(f"Time: {current_time}")
 
             # PUBLISH SETPOINT, do additional logic (kinematics) here
-            # servo_cmd = Float32MultiArray()
             servo_cmd = ServoCommand()
             for name, joint in self.joint_dict.items():
-                temp_setpoint = random.uniform(0, 30)
-                temp_setpoint += 150
-                # setattr(servo_cmd, name, joint.getSetpoint())
-                setattr(servo_cmd, name, temp_setpoint)
+                # Calculate sine wave for the setpoint with unique phase shift
+                temp_setpoint = (
+                    amplitude
+                    * math.sin(
+                        2 * math.pi * frequency * (current_time - last_time)
+                        + phase_shifts[name]
+                    )
+                    + 150
+                )
+                # Update the servo_cmd object
+                setattr(servo_cmd, name + "_setpoint", temp_setpoint)
 
             # Publish command to servo
             self.command_publisher.publish(servo_cmd)
@@ -78,16 +103,16 @@ class FirmwareNode:
                 setattr(servo_cmd, name, 150.0)
                 joint.setAngle(150.0)
             self.command_publisher.publish(servo_cmd)
-                
+
     def setJointAngle(self, name, angle):
         self.joint_dict[name].setAngle(angle)
-        
+
     def getJointPosition(self, name):
         return self.joint_dict[name].getPosition()
-    
+
     def getJointVelocity(self, name):
         return self.joint_dict[name].getVelocity()
-    
+
     def getJointLoad(self, name):
         return self.joint_dict[name].getLoad()
 
