e53fae1ad02d8ad42403148a277ba424ae6c095d
mn297 <peterwayne3293@gmail.com>
2024-07-21 14:13:12 -0400

    fixup! fixup! basic app beta version

--- teensy4_xl320_conf/src/old_driver/main_set_id.cpp
+++ teensy4_xl320_conf/src/old_driver/main_set_id.cpp
@@ -1,128 +1,128 @@
-#include "common.h"
-#if COMPILE_CFG == COMPILE_CFG_SET_ID_BAUD
-// ========================================
-// Dynamixel XL-320 Arduino library example
-// ========================================
+// #include "common.h"
+// #if COMPILE_CFG == COMPILE_CFG_SET_ID_BAUD
+// // ========================================
+// // Dynamixel XL-320 Arduino library example
+// // ========================================
 
-// Read more:
-// https://github.com/hackerspace-adelaide/XL320
-#include <Arduino.h>
+// // Read more:
+// // https://github.com/hackerspace-adelaide/XL320
+// #include <Arduino.h>
 
-#include "XL320.h"
+// #include "XL320.h"
 
-// Name your robot!
+// // Name your robot!
 
-#define SERVO_BAUD_LOW 9600
-#define SERVO_BAUD_HIGH 1000000
+// #define SERVO_BAUD_LOW 9600
+// #define SERVO_BAUD_HIGH 1000000
 
-XL320 robot;
-int servoID = 254;
-char rgb[] = "rgbypcwo";
+// XL320 robot;
+// int servoID = 254;
+// char rgb[] = "rgbypcwo";
 
-// Desired servo ID and baud rate
-int newServoID = 13;
-int newBaudRate = 3;
+// // Desired servo ID and baud rate
+// int newServoID = 13;
+// int newBaudRate = 3;
 
-void waitForEnter()
-{
-    SerialUSB.println("POWER CYCLE and PRESS ENTER to continue...");
-    // Clear the serial buffer
-    while (SerialUSB.available() > 0)
-    {
-        SerialUSB.read();
-    }
-    // Wait for the user to press enter
-    while (true)
-    {
-        if (SerialUSB.available() > 0)
-        {
-            char c = SerialUSB.read();
-            SerialUSB.print("Read char: "); // Debug print
-            SerialUSB.println(c);
-            if (c == '\n' || c == '\r')
-            {
-                SerialUSB.println("Enter detected, continuing...");
-                break;
-            }
-        }
-    }
-    SerialUSB.flush();
-}
+// void waitForEnter()
+// {
+//     SerialUSB.println("POWER CYCLE and PRESS ENTER to continue...");
+//     // Clear the serial buffer
+//     while (SerialUSB.available() > 0)
+//     {
+//         SerialUSB.read();
+//     }
+//     // Wait for the user to press enter
+//     while (true)
+//     {
+//         if (SerialUSB.available() > 0)
+//         {
+//             char c = SerialUSB.read();
+//             SerialUSB.print("Read char: "); // Debug print
+//             SerialUSB.println(c);
+//             if (c == '\n' || c == '\r')
+//             {
+//                 SerialUSB.println("Enter detected, continuing...");
+//                 break;
+//             }
+//         }
+//     }
+//     SerialUSB.flush();
+// }
 
-void setup()
-{
-    while (!SerialUSB)
-        ;
-    SerialUSB.begin(115200);
-    SerialUSB.println("XL-320 Set ID");
+// void setup()
+// {
+//     while (!SerialUSB)
+//         ;
+//     SerialUSB.begin(115200);
+//     SerialUSB.println("XL-320 Set ID");
 
-start_setup:
-    // Talking standard serial, so connect servo data line to Digital TX 1
-    // Set the default servo baud rate which is 1000000 (1Mbps) if it's a brand new servo
-    Serial1.begin(SERVO_BAUD_LOW, SERIAL_8N1_HALF_DUPLEX);
-    // Serial1.begin(9600, SERIAL_8N1_HALF_DUPLEX);
+// start_setup:
+//     // Talking standard serial, so connect servo data line to Digital TX 1
+//     // Set the default servo baud rate which is 1000000 (1Mbps) if it's a brand new servo
+//     Serial1.begin(SERVO_BAUD_LOW, SERIAL_8N1_HALF_DUPLEX);
+//     // Serial1.begin(9600, SERIAL_8N1_HALF_DUPLEX);
 
-    // Initialise your robot
-    robot.begin(Serial1); // Hand in the serial object you're using
+//     // Initialise your robot
+//     robot.begin(Serial1); // Hand in the serial object you're using
 
-    SerialUSB.printf("SETTING ID\r\n");
-    // ONE AT A TIME
-    // ================
-    // Set the servo ID
-    // ================
-    // writePacket(1, XL_ID, x) sets the ID:
-    // ID can be between 1 and 253 (but not 200)
-    robot.sendPacket(servoID, XL_ID, 10);
-    delay(200);
-    SerialUSB.printf("\tSet the servo ID to %d (9600)\r\n", newServoID);
-    robot.sendPacket(servoID, XL_ID, newServoID);
-    delay(200);
-    SerialUSB.println("\tChange the Serial1 baud rate to 1Mbps");
-    Serial1.begin(SERVO_BAUD_HIGH, SERIAL_8N1_HALF_DUPLEX); // Start with the new baud rate
-    delay(200);
-    SerialUSB.printf("\tSet the servo ID to %d (1Mbps)\r\n", newServoID);
-    robot.sendPacket(servoID, XL_ID, newServoID);
+//     SerialUSB.printf("SETTING ID\r\n");
+//     // ONE AT A TIME
+//     // ================
+//     // Set the servo ID
+//     // ================
+//     // writePacket(1, XL_ID, x) sets the ID:
+//     // ID can be between 1 and 253 (but not 200)
+//     robot.sendPacket(servoID, XL_ID, 10);
+//     delay(200);
+//     SerialUSB.printf("\tSet the servo ID to %d (9600)\r\n", newServoID);
+//     robot.sendPacket(servoID, XL_ID, newServoID);
+//     delay(200);
+//     SerialUSB.println("\tChange the Serial1 baud rate to 1Mbps");
+//     Serial1.begin(SERVO_BAUD_HIGH, SERIAL_8N1_HALF_DUPLEX); // Start with the new baud rate
+//     delay(200);
+//     SerialUSB.printf("\tSet the servo ID to %d (1Mbps)\r\n", newServoID);
+//     robot.sendPacket(servoID, XL_ID, newServoID);
 
-    // Prompt for enter key
-    waitForEnter();
-    Serial1.end();
-    SerialUSB.flush();
+//     // Prompt for enter key
+//     waitForEnter();
+//     Serial1.end();
+//     SerialUSB.flush();
 
-    SerialUSB.printf("SETTING BAUD\r\n");
-    // ===================================
-    // Set the serial connection baud rate
-    // ===================================
-    // writePacket(1, XL_BAUD_RATE, x) sets the baud rate:
-    // 0: 9600, 1:57600, 2:115200, 3:1Mbps
-    Serial1.begin(SERVO_BAUD_LOW, SERIAL_8N1_HALF_DUPLEX);
-    delay(200);
-    SerialUSB.println("\tSet the baud rate to 3 (9600)");
-    robot.sendPacket(servoID, XL_BAUD_RATE, 3);
-    delay(200);
-    SerialUSB.println("\tChange the Serial1 baud rate to 1Mbps");
-    Serial1.begin(SERVO_BAUD_HIGH, SERIAL_8N1_HALF_DUPLEX); // Start with the new baud rate
-    delay(200);
-    SerialUSB.println("\tSet the baud rate to 3 again (1Mbps)");
-    robot.sendPacket(servoID, XL_BAUD_RATE, 3);
+//     SerialUSB.printf("SETTING BAUD\r\n");
+//     // ===================================
+//     // Set the serial connection baud rate
+//     // ===================================
+//     // writePacket(1, XL_BAUD_RATE, x) sets the baud rate:
+//     // 0: 9600, 1:57600, 2:115200, 3:1Mbps
+//     Serial1.begin(SERVO_BAUD_LOW, SERIAL_8N1_HALF_DUPLEX);
+//     delay(200);
+//     SerialUSB.println("\tSet the baud rate to 3 (9600)");
+//     robot.sendPacket(servoID, XL_BAUD_RATE, 3);
+//     delay(200);
+//     SerialUSB.println("\tChange the Serial1 baud rate to 1Mbps");
+//     Serial1.begin(SERVO_BAUD_HIGH, SERIAL_8N1_HALF_DUPLEX); // Start with the new baud rate
+//     delay(200);
+//     SerialUSB.println("\tSet the baud rate to 3 again (1Mbps)");
+//     robot.sendPacket(servoID, XL_BAUD_RATE, 3);
 
-    robot.LED(newServoID, &rgb[random(0, 7)]);
+//     robot.LED(newServoID, &rgb[random(0, 7)]);
 
-    // Prompt for enter key
-    waitForEnter();
-    Serial1.end();
-    goto start_setup;
-}
+//     // Prompt for enter key
+//     waitForEnter();
+//     Serial1.end();
+//     goto start_setup;
+// }
 
-void loop()
-{
-    delay(1000);
-    // NOTE: load this sketch to the Arduino > Servo
-    // Then power cycle the Arduino + Servo
+// void loop()
+// {
+//     delay(1000);
+//     // NOTE: load this sketch to the Arduino > Servo
+//     // Then power cycle the Arduino + Servo
 
-    // NOTE: When setting the servo ID, the baud rate defaults down to 9600
-    // So then you'll need to power cycle, and re-write the baud rate to whatever you want via 9600
-    digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ 1);
-    // SerialUSB.println("Set ID to 10");
-    // robot.sendPacket(servoID, XL_ID, 10);
-}
-#endif // COMPILE_CFG == COMPILE_CFG_SET_ID
+//     // NOTE: When setting the servo ID, the baud rate defaults down to 9600
+//     // So then you'll need to power cycle, and re-write the baud rate to whatever you want via 9600
+//     digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ 1);
+//     // SerialUSB.println("Set ID to 10");
+//     // robot.sendPacket(servoID, XL_ID, 10);
+// }
+// #endif // COMPILE_CFG == COMPILE_CFG_SET_ID
--- teensy4_xl320_conf/src/old_driver/main_single_servo.cpp
+++ teensy4_xl320_conf/src/old_driver/main_single_servo.cpp
@@ -1,173 +1,173 @@
-#include "common.h"
-#if COMPILE_CFG == COMPILE_CFG_SINGLE_SERVO
-#include <Arduino.h>
-#include "cmd_utils.hpp"
+// #include "common.h"
+// #if COMPILE_CFG == COMPILE_CFG_SINGLE_SERVO
+// #include <Arduino.h>
+// #include "cmd_utils.hpp"
 
-#include "XL320.h"
-#include "HardwareSerial.h"
+// #include "XL320.h"
+// #include "HardwareSerial.h"
 
-#define DEBUG_PRINT 0
+// #define DEBUG_PRINT 0
 
-// SERVO CONFIGURATION
-// #define SERVO_BAUD 9600
-#define SERVO_BAUD 1000000
-XL320 robot;
-char rgb[] = "rgbypcwo";
-int servo_id = 13;
-int led_color = 0;
-float servo_setpoint_raw = 0;
-float servo_setpoint_deg = 0;
-float servo_pos_raw = 0;
-float servo_pos_deg = 0;
+// // SERVO CONFIGURATION
+// // #define SERVO_BAUD 9600
+// #define SERVO_BAUD 1000000
+// XL320 robot;
+// char rgb[] = "rgbypcwo";
+// int servo_id = 13;
+// int led_color = 0;
+// float servo_setpoint_raw = 0;
+// float servo_setpoint_deg = 0;
+// float servo_pos_raw = 0;
+// float servo_pos_deg = 0;
 
-void process_serial_cmd()
-{
-  static String inputString = "";             // A String to hold incoming data
-  static boolean inputStringComplete = false; // Whether the string is complete
+// void process_serial_cmd()
+// {
+//   static String inputString = "";             // A String to hold incoming data
+//   static boolean inputStringComplete = false; // Whether the string is complete
 
-  while (SerialUSB.available())
-  {
-    char inChar = (char)SerialUSB.read(); // Read each character
-    if (inChar == '\n')
-    {
-      inputStringComplete = true; // If newline, input is complete
-    }
-    else
-    {
-      inputString += inChar; // Add character to input
-    }
-  }
+//   while (SerialUSB.available())
+//   {
+//     char inChar = (char)SerialUSB.read(); // Read each character
+//     if (inChar == '\n')
+//     {
+//       inputStringComplete = true; // If newline, input is complete
+//     }
+//     else
+//     {
+//       inputString += inChar; // Add character to input
+//     }
+//   }
 
-  if (inputStringComplete)
-  {
-    SerialUSB.print("Received: ");
-    SerialUSB.println(inputString); // Echo the input for debugging
+//   if (inputStringComplete)
+//   {
+//     SerialUSB.print("Received: ");
+//     SerialUSB.println(inputString); // Echo the input for debugging
 
-    // Process the completed command
-    if (inputString.startsWith("i "))
-    {
-      // Increment command
-      float inc_deg = inputString.substring(2).toFloat(); // Extract number
-      servo_setpoint_raw = (servo_setpoint_raw + deg2raw(inc_deg));
-      SerialUSB.print("Incremented position by: ");
-      SerialUSB.println(inc_deg);
-    }
-    else if (inputString.startsWith("s "))
-    {
-      // Set command
-      servo_setpoint_deg = inputString.substring(2).toFloat(); // Extract and set new position
-      servo_setpoint_raw = deg2raw(servo_setpoint_deg);
-      SerialUSB.print("Set position to: ");
-      SerialUSB.println(servo_setpoint_deg);
-    }
-    else
-    {
-      SerialUSB.println("Unknown command");
-    }
+//     // Process the completed command
+//     if (inputString.startsWith("i "))
+//     {
+//       // Increment command
+//       float inc_deg = inputString.substring(2).toFloat(); // Extract number
+//       servo_setpoint_raw = (servo_setpoint_raw + deg2raw(inc_deg));
+//       SerialUSB.print("Incremented position by: ");
+//       SerialUSB.println(inc_deg);
+//     }
+//     else if (inputString.startsWith("s "))
+//     {
+//       // Set command
+//       servo_setpoint_deg = inputString.substring(2).toFloat(); // Extract and set new position
+//       servo_setpoint_raw = deg2raw(servo_setpoint_deg);
+//       SerialUSB.print("Set position to: ");
+//       SerialUSB.println(servo_setpoint_deg);
+//     }
+//     else
+//     {
+//       SerialUSB.println("Unknown command");
+//     }
 
-    // Clear the string for the next command
-    inputString = "";
-    inputStringComplete = false;
-  }
-}
+//     // Clear the string for the next command
+//     inputString = "";
+//     inputStringComplete = false;
+//   }
+// }
 
-void servo_setup()
-{
-  // Talking standard serial, so connect servo data line to Digital TX 1
-  // Comment out this line to talk software serial
-  Serial1.begin(SERVO_BAUD, SERIAL_8N1_HALF_DUPLEX);
+// void servo_setup()
+// {
+//   // Talking standard serial, so connect servo data line to Digital TX 1
+//   // Comment out this line to talk software serial
+//   Serial1.begin(SERVO_BAUD, SERIAL_8N1_HALF_DUPLEX);
 
-  // Initialise your robot
-  robot.begin(Serial1); // Hand in the serial object you're using
+//   // Initialise your robot
+//   robot.begin(Serial1); // Hand in the serial object you're using
 
-  // I like fast moving servos, so set the joint speed to max!
-  // robot.setJointSpeed(servo_id, 1023 / 2);
-  robot.setJointSpeed(servo_id, 1023 / 2);
+//   // I like fast moving servos, so set the joint speed to max!
+//   // robot.setJointSpeed(servo_id, 1023 / 2);
+//   robot.setJointSpeed(servo_id, 1023 / 2);
 
-  servo_setpoint_raw = 512;
-  servo_setpoint_deg = raw2deg(servo_setpoint_raw);
-}
+//   servo_setpoint_raw = 512;
+//   servo_setpoint_deg = raw2deg(servo_setpoint_raw);
+// }
 
-void servo_loop()
-{
-  // LED test.. let's randomly set the colour (0-7)
-  robot.LED(servo_id, &rgb[random(0, 7)]);
+// void servo_loop()
+// {
+//   // LED test.. let's randomly set the colour (0-7)
+//   robot.LED(servo_id, &rgb[random(0, 7)]);
 
-  // SETPOINT TEST
-  robot.moveJoint(servo_id, servo_setpoint_raw);
-  delay(100);
-  //  Serial1.clear();
-  byte buffer[256];
-  XL320::Packet p = XL320::Packet(buffer, robot.readPacket(buffer, 256));
+//   // SETPOINT TEST
+//   robot.moveJoint(servo_id, servo_setpoint_raw);
+//   delay(100);
+//   //  Serial1.clear();
+//   byte buffer[256];
+//   XL320::Packet p = XL320::Packet(buffer, robot.readPacket(buffer, 256));
 
-  // Driver diagnostic
-#if DEBUG_PRINT == 1
-  p.toStream(SerialUSB);
-#endif
+//   // Driver diagnostic
+// #if DEBUG_PRINT == 1
+//   p.toStream(SerialUSB);
+// #endif
 
-  delay(100);
-  Serial1.clear();
+//   delay(100);
+//   Serial1.clear();
 
-  // Get state
-  // servo_pos_raw = robot.getJointPosition(servo_id);
-  servo_pos_raw = robot.getJointPosition(servo_id, &SerialUSB);
+//   // Get state
+//   // servo_pos_raw = robot.getJointPosition(servo_id);
+//   servo_pos_raw = robot.getJointPosition(servo_id, &SerialUSB);
 
-  // Get all IDs
-  byte idBuf[16];
-  int numIDs = robot.broadcastPing(&SerialUSB, idBuf);
-  if (numIDs)
-  {
-    SerialUSB.print(numIDs);
-    SerialUSB.print(" IDs found: ");
-    for (int i = 0; i < numIDs; i++)
-    {
-      SerialUSB.print(idBuf[i]);
-      SerialUSB.print(",");
-    }
-    SerialUSB.println();
-  }
-  else
-  {
-    SerialUSB.println("No IDs found.");
-  }
+//   // Get all IDs
+//   byte idBuf[16];
+//   int numIDs = robot.broadcastPing(&SerialUSB, idBuf);
+//   if (numIDs)
+//   {
+//     SerialUSB.print(numIDs);
+//     SerialUSB.print(" IDs found: ");
+//     for (int i = 0; i < numIDs; i++)
+//     {
+//       SerialUSB.print(idBuf[i]);
+//       SerialUSB.print(",");
+//     }
+//     SerialUSB.println();
+//   }
+//   else
+//   {
+//     SerialUSB.println("No IDs found.");
+//   }
 
-  servo_pos_deg = raw2deg(servo_pos_raw);
+//   servo_pos_deg = raw2deg(servo_pos_raw);
 
-  delay(100);
-}
+//   delay(100);
+// }
 
-uint32_t last_print = 0;
-void print_servo_state()
-{
-  // Print the servo state
-  SerialUSB.print("Servo ID: ");
-  SerialUSB.print(servo_id);
-  SerialUSB.print(", setpoint_deg: ");
-  SerialUSB.print(servo_setpoint_deg);
-  SerialUSB.print(", pos_deg: ");
-  SerialUSB.print(servo_pos_deg);
-  SerialUSB.print(", setpoint_raw: ");
-  SerialUSB.print(servo_setpoint_raw);
-  SerialUSB.print(", pos_raw: ");
-  SerialUSB.println(servo_pos_raw);
-}
+// uint32_t last_print = 0;
+// void print_servo_state()
+// {
+//   // Print the servo state
+//   SerialUSB.print("Servo ID: ");
+//   SerialUSB.print(servo_id);
+//   SerialUSB.print(", setpoint_deg: ");
+//   SerialUSB.print(servo_setpoint_deg);
+//   SerialUSB.print(", pos_deg: ");
+//   SerialUSB.print(servo_pos_deg);
+//   SerialUSB.print(", setpoint_raw: ");
+//   SerialUSB.print(servo_setpoint_raw);
+//   SerialUSB.print(", pos_raw: ");
+//   SerialUSB.println(servo_pos_raw);
+// }
 
-void setup()
-{
-  SerialUSB.begin(115200);
-  while (!SerialUSB)
-    ;
-  servo_setup();
-}
+// void setup()
+// {
+//   SerialUSB.begin(115200);
+//   while (!SerialUSB)
+//     ;
+//   servo_setup();
+// }
 
-void loop()
-{
-  process_serial_cmd();
-  servo_loop();
-  if (millis() - last_print > 500)
-  {
-    print_servo_state();
-    last_print = millis();
-  }
-}
-#endif // COMPILE_CFG == COMPILE_CFG_SINGLE_SERVO
+// void loop()
+// {
+//   process_serial_cmd();
+//   servo_loop();
+//   if (millis() - last_print > 500)
+//   {
+//     print_servo_state();
+//     last_print = millis();
+//   }
+// }
+// #endif // COMPILE_CFG == COMPILE_CFG_SINGLE_SERVO
